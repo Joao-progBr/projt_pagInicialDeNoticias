127e551dcdc29b07ce533b115a58246d0f537ce4
joãoA <joaovictor.dudu03@gmail.com>
2025-06-27 23:57:09 -0300

    melhorado o responsivo, adicionando hover no title do article e aside

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,17 @@
 const openMenu = document.getElementById('openMenu')
 const closeMenu = document.getElementById('closeMenu')
 const mobileMenu = document.getElementById('mobileMenu')
+const body = document.body
 
 openMenu.addEventListener('click', function(){
     mobileMenu.classList.add('active')
     // openMenu.style.display = 'none'
+    body.classList.add('no-scroll')
 })
 
 closeMenu.addEventListener('click', function(){
     mobileMenu.classList.remove('active')
     // openMenu.style.display = 'none'
+    body.classList.remove('no-scroll')
 })
 
